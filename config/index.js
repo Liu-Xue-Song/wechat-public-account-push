e2d6dbbb1edca458ee7e2beb1ae982bac18332d6
Liu-Xue-Song <45343592+Liu-Xue-Song@users.noreply.github.com>
2022-08-29 10:49:32 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ export const config = {
     // 模板消息id
     TEMPLATE_ID: "Hb-FJGaVxFnMT1AZ40nCN9TUr-qqX2QzAr_5TsdMzpU",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: ["o2ez453zL_MjwYH9_ka3aQDIDYE4"],
+    USERS: ["o2ez453zL_MjwYH9_ka3aQDIDYE4","o2ez45zmUQb784k9sbTJ8lwZCEGc"],
      
     /**
      * 信息配置
